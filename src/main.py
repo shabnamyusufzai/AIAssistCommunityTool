f945fb6de7d066ac497c2d674cddc8624412d758
shabnamyusufzai <shabmyusufzai@gmail.com>
2025-12-09 12:23:11 -0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,29 @@
+def main():
+    os.environ["OPENAI_API_KEY"] = ""
+    resources = json.load(open("IssuesAndLinks_Flattened.json"))
+    userInput = "I need somewhere to sleep tonight."
+    document = []
+    for resource in resources:
+        document.append(convertJSONToText(resource))
+
+    fIndex = embedAndFaiss(document)
+
+    def returnOutput(userIn, history):
+        topIssueLinks = retrieveClosestIssueAssociated(document,userIn,fIndex)
+        result = promptModel(userIn, topIssueLinks)
+        history = []
+        history.append((userIn, result))
+        return history, ""
+
+    with gradio.Blocks() as demo:
+        chatbot = gradio.Chatbot(height=600)
+        userInput = gradio.Textbox(placeholder="What can I help you with?")
+        userInput.submit(
+            returnOutput, 
+            [userInput, chatbot],
+            [chatbot, userInput]
+        )
+    demo.launch()
+  
+if __name__ == "__main__":
+    main()
