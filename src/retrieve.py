228b546ef36c60a34eb91835afa389fc042b40e0
shabnamyusufzai <shabmyusufzai@gmail.com>
2025-12-09 12:41:23 -0500

    Create retrieve.py

---
+++ src/retrieve.py
@@ -0,0 +1,10 @@
+from embeddings import model
+
+def retrieveClosestIssueAssociated(document, userRequest, faissIndex, k=3):
+    closest = []
+    embeddedUserRequest = model.encode([userRequest])
+    distance, ind = faissIndex.search(embeddedUserRequest, k)
+    ind = ind[0]
+    for i in ind:
+        closest.append(document[i])
+    return closest
